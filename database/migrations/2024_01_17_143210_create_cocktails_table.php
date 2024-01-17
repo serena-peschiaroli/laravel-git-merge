038eb25c30be6b03658544cf39bcb09880a0f1aa
serena-peschiaroli <serena.peschiaroli@gmail.com>
2024-01-17 15:39:02 +0100

    Add: db table

---
+++ database/migrations/2024_01_17_143210_create_cocktails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cocktails', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->boolean('e_alcolico')->default(true);
+            $table->decimal('prezzo')->unsigned();
+            $table->text('descrizione');
+            $table->string('colore');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cocktails');
+    }
+};
