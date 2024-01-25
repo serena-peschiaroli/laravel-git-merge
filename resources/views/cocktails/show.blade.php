a7b6f0995aae96a3e03920267eb364bc98c2711f
serena-peschiaroli <serena.peschiaroli@gmail.com>
2024-01-25 16:03:17 +0100

    Add: show view

--- resources/views/cocktails/show.blade.php
+++ resources/views/cocktails/show.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 
-ciao
+<div class="container">
+    <a href="{{route('cocktails.index')}}" class="btn btn-primary">Indietro</a>
+    <div class="card">
+        <h5 class="card-title"><strong>Nome: </strong> {{ $cocktail->nome}}</h5>
+        <h4 class="card-subtitle mb-2 text-muted"> <strong>Alcolico: </strong> {{ $cocktail->e_alcolico ?  'sì' : 'no'}} </h4>
+        <div class="card-body">
+            <p class="card-text"> <strong>Descrizione: </strong> {{ $cocktail->descrizione}}</p>
+            <p class="card-text"> <strong>Colore: </strong> {{ $cocktail->colore}}</p>
+            <p class="card-text"> <strong>Prezzo: </strong> {{ $cocktail->prezzo}}</p>
+        </div>
+        <div class="btn-wrapper">
+            
+            
+        </div>
+        
+    </div>
+</div>
     
 @endsection
